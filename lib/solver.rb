061eb95df8c9c4c23abae5aa1e98766e89bac2eb
Samir <samirsoormally@gmail.com>
2018-12-09 16:19:37 +0000

    feat: user can find mixed case anagrams

--- lib/solver.rb
+++ lib/solver.rb
@@ -20,6 +20,6 @@ class Solver
   private
   
   def normalise(word)
-    word.chars.sort.join
+    word.downcase.chars.sort.join
   end
 end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -1,12 +1,21 @@
 require "./lib/solver.rb"
 
 describe "solver" do
-  subject(:solver) { Solver.new('./assets/test_wordlist.txt') }
+  subject(:solver) { Solver.new('./assets/test_wordlist.txt', mock_printer) }
+  let(:mock_printer) { double('Printer', show_anagrams: []) }
 
     describe "#find_anagrams" do
+      
       it "shows lower-case character anagrams" do
         solver.find_anagrams('monkey')
+        
         expect(solver.anagrams).to eq(['monkey', 'omnkey', 'ynkemo'])
+      end
+      
+      it "shows mixed-case character anagrams" do
+        solver.find_anagrams('elephant')
+        
+        expect(solver.anagrams).to eq(['Elephant', 'phaNteLe', 'elphAnte'])
       end
     end
  end
